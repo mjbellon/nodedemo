3f1e04ea6507a7d3c0663c67fe3ae5ef8b09c511
mike <mike@dje-automation.com>
2014-12-06 17:55:14 -0800

    Initial skeleton framework - from Mean web dev

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 /**
- * New node file
+ * main node start file
  */
-var express = require('express'); 
 
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var app = express(); 
-app.set('domain', 'http://54.149.107.120/');
+var pg = require('./config/pg'),
+	express = require('./config/express'),
+	passport = require('./config/passport');
 
-app.use('/', function( req, res) { 
-	res.send('Hello World'); 
-	}); 
-var listener = app.listen(1337); 
-console.log('Server running somewhere at ' + listener.address().address); 
-console.log(listener.address().port);
+//var db = mongoose();
+var db = pg();
+var app = express(db);
+var passport = passport();
+
+app.listen(1337); 
 
 module.exports = app;
+console.log('Server running at http://localhost:1337/');
